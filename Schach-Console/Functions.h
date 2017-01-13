bf8b7ab1b038968b2d6bdfd21229b7d0a3c5b0d0
Sebastian Mischke <SebastianMischke@gmx.de>
2017-01-13 22:31:48 +0100

    Add foreach as macro

--- Schach-Console/Functions.h
+++ Schach-Console/Functions.h
@@ -5,5 +5,13 @@ typedef int bool;
 #define true 1
 #define false 0
 
+#define foreach(item, array) \
+    for(int keep = 1, \
+            count = 0,\
+            size = sizeof (array) / sizeof *(array); \
+        keep && count != size; \
+        keep = !keep, count++) \
+      for(item = (array) + count; keep; keep = !keep)
+
 void gotoXY(int x, int y);
 int getArrowKey(void);
